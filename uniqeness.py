34ef71b3e116552e5164671dad5e20f25c8da04e
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-12-01 14:26:36 +0300

    y

--- uniqeness.py
+++ uniqeness.py
@@ -24,7 +24,6 @@ crucial in order to make a decision regarding which new interval to consider in 
 directory_path = "data/SNAP_facebook_clean_data"
 
 
-
 def readNetworkFromRLD(directory_path):
     edges = []
     for filename in os.listdir(directory_path):
